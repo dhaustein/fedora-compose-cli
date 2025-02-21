6bb067dc160766e59bb21ba70747db79bd221f94
Dusan Haustein <dhaustei@redhat.com>
2025-02-21 18:06:39 +0100

    refactor

--- main.py
+++ main.py
@@ -1,81 +1,161 @@
 import argparse
 from concurrent.futures import ProcessPoolExecutor
-from difflib import SequenceMatcher
+from dataclasses import dataclass
+from functools import partial
 from time import perf_counter
-from typing import Dict, List
+from typing import Callable, Dict, Iterator, List, MutableSet, Set, TypeVar
 
 import ijson
 
+# TODO change into config file
 PREFIX: str = "payload.rpms.Everything.x86_64"
 
 
-def parse_rpm_json_file(filepath: str, prefix: str) -> set[str]:
+@dataclass
+class Package:
+    name: str
+    full_name: str
+    epoch: str
+    version: str
+    release: str
+    distro_version: str
+    arch: str
+
+    def __str__(self) -> str:
+        return self.full_name
+
+    def __hash__(self) -> int:
+        return hash(self.full_name)  # Full name should be unique
+
+    # def __eq__(self, other: object) -> bool:
+    #     if not isinstance(other, Package):
+    #         return NotImplemented
+    #     return self.full_name == other.full_name
+
+
+T = TypeVar("T", bound=Package)
+
+
+class PackageSet(MutableSet[T]):
+    def __init__(self, items: Set[T] | None = None) -> None:
+        self._items: Set[T] = set() if items is None else set(items)
+
+    def __contains__(self, item: object) -> bool:
+        return item in self._items
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self._items)
+
+    def __len__(self) -> int:
+        return len(self._items)
+
+    def add(self, item: T) -> None:
+        self._items.add(item)
+
+    def discard(self, item: T) -> None:
+        self._items.discard(item)
+
+    def intersection(self, other: "PackageSet[T]") -> "PackageSet[T]":
+        return PackageSet(self._items.intersection(other._items))
+
+    def difference(self, other: "PackageSet[T]") -> "PackageSet[T]":
+        return PackageSet(self._items.difference(other._items))
+
+
+def parse_rpm_json_file(filepath: str, prefix: str, get_pkg: Callable) -> PackageSet:
+    payloads: PackageSet[Package] = PackageSet()
     # TODO use path from Pathlib instead of str
-    payloads = set()
     with open(filepath, "br") as f:
         pkgs = ijson.kvitems(f, prefix, use_float=True)
         for k, v in pkgs:
-            payloads.add(k)
+            payloads.add(get_pkg(k))
 
     return payloads
 
 
-def parse_pkg_nevra(pkg: str) -> Dict[str, str]:
-    """Parse package NEVRA string into component parts.
+def get_pkg(pkg: str) -> Package:
+    """Parse package NEVRA string into component parts and cosntruct Package object
 
     Format: {name}-{epoch}:{pkg_ver}-{pkg_rls}.{distro_rls}.{arch}
-    Example: rust-uuid-1:1.13.2-1.fc43.src ->
-    {name: rust-uuid, epoch: 1, version: 1.13.2, release: 1, fedora_version: fc43, arch: src}
+
+    Example: rust-uuid-1:1.13.2-1.fc43.src will results into:
+
+    Package(
+        name="rust-uuid",
+        full_name=rust-uuid-1:1.13.2-1.fc43.src,
+        epoch="1",
+        version="1.13.2",
+        release="4",
+        distro_version="fc43",
+        arch="src",
+    )
     """
-    # Split distro version and arch
-    nev, distro_rls, arch = pkg.rsplit(".", 2)
+    print(f"\n{pkg}")
+
+    # Split arch first
+    pkg_base, arch = pkg.rsplit(".", 1)
+
+    # Try to split distro version if it exists otherwise set it to empty string
+    parts = pkg_base.rsplit(".", 1)
+    if len(parts) > 1:
+        nev, distro_rls = parts
+    else:
+        nev = parts[0]
+        distro_rls = ""
+
     # Split name, epoch:version, and release
+    print(f"pkg_base: {pkg_base}")
+    print(f"arch: {arch}")
+    print(f"distro_rls: {distro_rls}")
+    print(f"nev: {nev}")
     name, epoch_ver, pkg_rls = nev.rsplit("-", 2)
+
+    print(f"name: {name}")
+    print(f"epoch: {epoch_ver}")
+    print(f"version: {pkg_rls}")
     # Split epoch and pkg version
     epoch, pkg_ver = epoch_ver.split(":")
 
-    return {
-        "name": name,
-        "epoch": epoch,
-        "version": pkg_ver,
-        "release": pkg_rls,
-        "fedora_version": distro_rls,
-        "arch": arch,
-    }
+    return Package(
+        name=name,
+        full_name=pkg,
+        epoch=epoch,
+        version=pkg_ver,
+        release=pkg_rls,
+        distro_version=distro_rls,
+        arch=arch,
+    )
 
 
-def get_nochanged_pkgs(old: set[str], new: set[str]) -> set[str]:
+def get_nochanged_pkgs(old: PackageSet, new: PackageSet) -> PackageSet:
     return old.intersection(new)
 
 
-def get_added_pkgs(old: set[str], new: set[str]) -> set[str]:
-    return new - old
+def get_added_pkgs(old: PackageSet, new: PackageSet) -> PackageSet:
+    return new.difference(old)
 
 
-def get_removed_pkgs(old: set[str], new: set[str]) -> set[str]:
-    return old - new
+def get_removed_pkgs(old: PackageSet, new: PackageSet) -> PackageSet:
+    return old.difference(new)
 
 
 def get_changed_pkgs(
-    removed_pkgs: set[str], added_pkgs: set[str]
-) -> Dict[str, List[str]]:
-    changed: Dict[str, List[str]] = {"old": [], "new": []}
+    removed_pkgs: PackageSet, added_pkgs: PackageSet
+) -> Dict[str, List[Package]]:
+    changed: Dict[str, List[Package]] = {"old": [], "new": []}
 
-    for old_item in removed_pkgs:
-        for new_item in added_pkgs:
+    for removed in removed_pkgs:
+        for added in added_pkgs:
             if (
-                # TODO parse and filter the %name out and compare similarity of %version only
-                # %{name}-%{version}-%{release}.%{arch} do not rely on .ratio()
-                # does not recognize changes like this:
-                # rust-uuid-0:1.11.0-2.fc42.src -> rust-uuid-0:1.13.2-1.fc43.src
-                # python-trio-websocket-0:0.12.0~dev^202501304247cd5-1.fc43.src -> python-trio-websocket-0:0.12.1-1.fc43.src
-                SequenceMatcher(None, old_item, new_item).ratio()
-                > 0.9
+                removed["epoch"] != added["epoch"]
+                or removed["version"] != added["version"]
+                or removed["release"] != added["release"]
+                or removed["fedora_version"] != added["fedora_version"]
             ):
-                changed["old"].append(old_item)
-                changed["new"].append(new_item)
                 # TODO order matters here, probably not a great idea to blindly add each side to a list
                 # without checking if the item has the correct counterpart
+                changed["old"].append(removed)
+                changed["new"].append(added)
 
     return changed
 
@@ -87,21 +167,28 @@ def main():
     )
     parser.add_argument(dest="old_rpm", help="Filepath to old RPM JSON file")
     parser.add_argument(dest="new_rpm", help="Filepath to new RPM JSON file")
+    parser.add_argument(
+        "-p",
+        "--prefix",
+        dest="json_prefix",
+        default=PREFIX,
+        help="The ijson prefix used to specify the JSON object to parse from the files, default: payload.rpms.Everything.x86_64",
+    )
     args = parser.parse_args()
 
-    # Parse both JSON files and create a set of all packages for old and new version
+    # Parse both JSON files in parallel and create two sets of Packages for old and new composes both
+    parse_and_get_pkg = partial(parse_rpm_json_file, get_pkg=get_pkg)
     with ProcessPoolExecutor() as pool:
         result = pool.map(
-            parse_rpm_json_file,
-            # ["compose_metadata/rpms_1802.json", "compose_metadata/rpms_1902.json"],
+            parse_and_get_pkg,
             [args.old_rpm, args.new_rpm],
-            [PREFIX, PREFIX],
+            [args.json_prefix, args.json_prefix],
         )
-    old_rpms, new_rpms = result
+    old_pkgs, new_pkgs = result
 
-    removed = get_removed_pkgs(old_rpms, new_rpms)
-    added = get_added_pkgs(old_rpms, new_rpms)
-    changed = get_changed_pkgs(removed, added)
+    removed = get_removed_pkgs(old_pkgs, new_pkgs)
+    added = get_added_pkgs(old_pkgs, new_pkgs)
+    changed = get_changed_pkgs(removed, added, get_pkg)
 
     # Get only packages that have been completely removed
     completely_removed = [
@@ -112,7 +199,7 @@ def main():
         i for i in added if i not in set(changed["new"]).union(changed["old"])
     ]
 
-    # TODO pretty output should be templated
+    # TODO output should be templated in a separate module
     print(f"REMOVED: {completely_removed}")
     print(f"ADDED: {completely_new}")
     print(f"CHANGED: {changed}")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,27 +1,59 @@
 import pytest
 
-from main import parse_pkg_nevra
+from main import Package, get_pkg
 
 
 def test_parse_dev_pkg():
     pkg_name = "python-trio-websocket-0:0.12.0~dev^202501304247cd5-1.fc43.src"
-    assert parse_pkg_nevra(pkg_name) == {
-        "name": "python-trio-websocket",
-        "epoch": "0",
-        "version": "0.12.0~dev^202501304247cd5",
-        "release": "1",
-        "fedora_version": "fc43",
-        "arch": "src",
-    }
+    expected_pkg = Package(
+        name="python-trio-websocket",
+        full_name=pkg_name,
+        epoch="0",
+        version="0.12.0~dev^202501304247cd5",
+        release="1",
+        distro_version="fc43",
+        arch="src",
+    )
+    assert get_pkg(pkg_name) == expected_pkg
 
 
 def test_parse_pkg():
-    pkg_name = "rust-uuid-1:1.13.2-1.fc43.src"
-    assert parse_pkg_nevra(pkg_name) == {
-        "name": "rust-uuid",
-        "epoch": "1",
-        "version": "1.13.2",
-        "release": "1",
-        "fedora_version": "fc43",
-        "arch": "src",
-    }
+    pkg_name = "rust-uuid-1:1.13.2-4.fc43.src"
+    expected_pkg = Package(
+        name="rust-uuid",
+        full_name=pkg_name,
+        epoch="1",
+        version="1.13.2",
+        release="4",
+        distro_version="fc43",
+        arch="src",
+    )
+    assert get_pkg(pkg_name) == expected_pkg
+
+
+def test_parse_dummy_test_pkg():
+    pkg_name = "dummy-test-package-crested-0:0-3771.src"
+    expected_pkg = Package(
+        name="dummy-test-package-crested",
+        full_name=pkg_name,
+        epoch="0",
+        version="0",
+        release="3771",
+        distro_version="",  # no distro version for this pkg
+        arch="src",
+    )
+    assert get_pkg(pkg_name) == expected_pkg
+
+
+def test_parse_shim_pkg():
+    pkg_name = "shim-0:15.8-3.src"
+    expected_pkg = Package(
+        name="shim",
+        full_name=pkg_name,
+        epoch="0",
+        version="15.8",
+        release="3",
+        distro_version="",  # no distro version for this pkg
+        arch="src",
+    )
+    assert get_pkg(pkg_name) == expected_pkg
